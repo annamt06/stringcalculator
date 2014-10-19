c77d5cea2cb98ff899845d4db6e9b2a957aff897
Annamaria <annamt06@ru.is>
2014-10-19 00:19:48 +0000

    Part 3, test, new line test

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -28,9 +28,19 @@ public class CalculatorTest {
     public void testMultipleNumbers(){
     	assertEquals(6, Calculator.add("1,2,3"));
     }
+    
     @Test
     public void testTenNumbers(){
     	assertEquals(55, Calculator.add("1,2,3,4,5,6,7,8,9,10"));
     }
 
+    @Test
+    public void testNewLines(){
+    	assertEquals(3, Calculator.add("1\n2"));
+    }
+
+    @Test
+    public void testNewLinesMultiNumbers(){
+    	assertEquals(6, Calculator.add("1\n2,3"));
+    }
 }
